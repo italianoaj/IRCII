bf4d51c47abc30df4eac16ce9335d71a2de75227
Anthony Italiano <italianoaj@Anthonys-MacBook-Air.local>
2019-10-30 13:51:28 -0400

    added comments to test_pam

--- test_pam.c
+++ test_pam.c
@@ -1,28 +1,36 @@
-//gcc -o test_pam test_pam.c -lpam -lpam_misc
+/* This program is used to test the PAM module before placing it into a live enviroment.
+The program will test all of the functions within the italiano.c program. */
+// @author italianoaj //
+
+//Include statements
 #include <stdio.h>
 #include <stdlib.h>
 #include <security/pam_appl.h>
 #include <security/pam_misc.h>
 
+//Conversation structure
 static struct pam_conv conv={
 	misc_conv,
 	NULL
 };
 
+//The main function will run the tests of the italiano.c program.
 int main(){
 
 	pam_handle_t *handle = NULL;
+        //name of the file inside of the /etc/pam.d direcotry to look for PAM rules
         const char *service_name = "test_pam";
         int retval;
-        char *username; /* This will be set by PAM with pam_get_item (see below) */
+        char *username;
 
-        retval = pam_start(service_name, NULL, &conv, &handle); /* Initializing PAM */
+        //Initialize PAM 
+        retval = pam_start(service_name, NULL, &conv, &handle);
         if (retval != PAM_SUCCESS){
                 fprintf(stderr, "Failure in pam initialization: %s\n", pam_strerror(handle, retval));
                 return 1;
         }
-
-        retval = pam_authenticate(handle, 0); /* Do authentication (user will be asked for username and password)*/
+        //Run authentication, retrieve username and password from user
+        retval = pam_authenticate(handle, 0);
         if (retval != PAM_SUCCESS) {
                 fprintf(stderr, "Failure in pam authentication: %s\n", pam_strerror(handle, retval));
                 return 1;
@@ -33,38 +41,21 @@ int main(){
                 fprintf(stderr, "Failure in pam account management: %s\n", pam_strerror(handle, retval));
                 return 1;
        }
-
+        //Open the session for the user and mount the home directory.
 	retval=pam_open_session(handle, 0);
 	if(retval!=PAM_SUCCESS){
 		fprintf(stderr, "Failure in opening session: %s\n", pam_strerror(handle, retval));
 		return 1;
 	}
-
+        //Set the user's full name as an ENV variable 
 	retval=pam_setcred(handle, 0);
 	if(retval!=PAM_SUCCESS){
 		fprintf(stderr, "Failure in setting user variable: %s\n", pam_strerror(handle, retval));
 		return 1;
 	}
 
-        /* We now get the username given by the user */
+        //Retrieve the username entered by the user.
         pam_get_item(handle,PAM_USER,(const void **)&username);
-
-        printf("Do you want to change your password? (answer y/n): ");
-        char answer = getc(stdin); /* Taking user answer */
-        if (answer == 'y') {
-                retval = pam_chauthtok(handle, 0); /* Do update (user will be asked for current and new password) */
-                if (retval != PAM_SUCCESS) {
-                        fprintf(stderr, "Failure in pam password: %s", pam_strerror(handle, retval));
-                        return 1;
-                }
-        }else{
-		printf("okay, bye-bye\n");
-		retval=pam_close_session(handle,0);
-		if(retval!=PAM_SUCCESS){
-			fprintf(stderr, "Failure in closing session: %s", pam_strerror(handle, retval));
-			return 1;
-		}
-	}
 
         pam_end(handle, retval); /* ALWAYS terminate the pam transaction!! */
 }
