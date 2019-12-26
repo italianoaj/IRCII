6355c7a8682c2901a74a6a5d66bfdc46e95db229
Anthony Italiano <italianoaj@gcc.neo.rr.com>
2019-12-26 14:33:30 -0500

    Started enrollment program

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CC = gcc
 FLAGS_TEST = -Wall -lpam -lpam_misc
 FLAGS_ITALIANO = -fPIC -fno-stack-protector -c
+WARN = -Wall
 
-all: tester italiano italianoMod
+all: tester italiano italianoMod enroll
 
 tester:
 	$(CC) -o tester test_pam.c $(FLAGS_TEST)
@@ -13,5 +14,8 @@ italiano:
 italianoMod:
 	sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/italiano.so italiano.o
 
+enroll:
+	$(CC) -o otp-enroll otp-enroll.c $(WARN)
+
 clean:
-	-rm -f tester italiano.o
+	-rm -f tester italiano.o otp-enroll
---
+++ otp-enroll.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char error_message[30]="An Error has occured\n";
+char *number;
+char *name;
+char *username;
+char *tmp;
+char *tmp2;
+char *tmp3;
+char *line_buf=NULL;
+char *line_buf2=NULL;
+char *line_buf3=NULL;
+size_t line_buf_size=0;
+FILE *fp;
+
+int main(){
+	printf("Enter your fullname: ");
+	getline(&line_buf,&line_buf_size,stdin);
+	name=strtok_r(line_buf,"\n",&tmp);
+	//printf("You entered %s\n", name);
+	printf("Enter your username: ");
+	getline(&line_buf2,&line_buf_size,stdin);
+	username=strtok_r(line_buf2,"\n",&tmp2);
+	//printf("You entered %s\n", username);
+	printf("Carrier extensions:\nAlltel - mms.alltelwireless.com\nAT&T - mms.att.net\nBoost Mobile - myboostmobile.com\nCricket Wireless - mms.att.net\nMetroPCS - mymetropcs.com\nProject Fi - msg.fi.google.com\nSprint - pm.sprint.com\nStraight Talk - mms.att.net\nT-Mobile - tmomail.net\nVerizon - vzwpix.com\nVirgin Mobile - vmobl.com\n\n");
+	printf("Please enter your phone number follwoinf the correct carrier extension. Example - 1234567890@mms.att.net: ");
+	getline(&line_buf3,&line_buf_size,stdin);
+	number=strtok_r(line_buf3,"\n",&tmp3);
+	//printf("You entered %s\n", number);
+	char *line=malloc(strlen(name+1)+1+strlen(username+1)+1+strlen(number+1));
+	strcat(line,name);
+	strcat(line, ":");
+	strcat(line, username);
+	strcat(line, ":");
+	strcat(line, number);
+	printf("Line to be entered is\n%s\n", line);
+	return 0;
+}
