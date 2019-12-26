5a4b542a849904dad3b8b1f73cc149c1c2ed2851
Anthony Italiano <italianoaj@gcc.neo.rr.com>
2019-12-26 15:14:50 -0500

    otp-enroll adds user information to usrf - thats it

--- otp-enroll.c
+++ otp-enroll.c
@@ -14,6 +14,9 @@ char *line_buf2=NULL;
 char *line_buf3=NULL;
 size_t line_buf_size=0;
 FILE *fp;
+char buffer[64];
+
+#define UF "/etc/italiano_verify/usrf"
 
 int main(){
 	printf("Enter your fullname: ");
@@ -29,12 +32,21 @@ int main(){
 	getline(&line_buf3,&line_buf_size,stdin);
 	number=strtok_r(line_buf3,"\n",&tmp3);
 	//printf("You entered %s\n", number);
-	char *line=malloc(strlen(name+1)+1+strlen(username+1)+1+strlen(number+1));
+	char *line=malloc(strlen(name+1)+1+strlen(username+1)+1+strlen(number+1)+1);
 	strcat(line,name);
 	strcat(line, ":");
 	strcat(line, username);
 	strcat(line, ":");
 	strcat(line, number);
-	printf("Line to be entered is\n%s\n", line);
+	strcat(line, "\n");
+	//printf("Line to be entered is\n%s\n", line);
+	fp=fopen(UF,"a");
+	if(!fp){
+		printf("Unable to open %s\n",UF);
+		return 1;
+	}
+	fputs(line,fp);
+	fclose(fp);
+	free(line);
 	return 0;
 }
